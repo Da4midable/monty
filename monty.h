daa02a97f95ceab9dd9ef4e2aa39dfaa7cafe749
Saheed Kehinde Yusuf <108232561+Da4midable@users.noreply.github.com>
2023-11-16 11:15:17 +0100

    Pop removes value at the top of the stack

--- monty.h
+++ monty.h
@@ -1,7 +1,6 @@
 #ifndef MONTY_H
 #define MONTY_H
 
-#include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -46,9 +45,10 @@ typedef struct instruction_s
 } instruction_t;
 
 
-
+void pop(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t *stack);
 void push(stack_t **stack, int n, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
+
 #endif
